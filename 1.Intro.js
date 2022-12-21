a9e80b2e158d0294840940e6cc6c3c4e2a8082d6
amir-khaleghi <a.khaleghi.d@gmail.com>
2022-12-21 11:59:01 +0330

    Blocking-code

--- 1.Intro.js
+++ 1.Intro.js
@@ -164,6 +164,7 @@ readFile("./Content/first.txt", "utf8", (err, result) => {
 /* ------------------------------------------------------ */
 /*            compare Synch and Async Approach            */ // #09
 /* ------------------------------------------------------ */
+
 /* ------------------------ Sync ------------------------ */
 const { readFileSync, writeFileSync } = require("fs");
 console.log("start");
@@ -177,7 +178,6 @@ const create = readFileSync("./content/create.txt", "utf8");
 console.log("done with this task");
 console.log("starting the next one");
 
-/* ------------------------------------------------------ */
 /* ------------------------ Async ----------------------- */
 const { readFile, writeFile } = require("fs");
 console.log("start");
@@ -213,3 +213,105 @@ readFile("./Content/first.txt", "utf8", (err, result) => {
 });
 // after the first readfile
 console.log("starting next task");
+
+/* ------------------------------------------------------ */
+/*                          HTTP                          */ //#10
+/* ------------------------------------------------------ */
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log(req); // request is a giant object
+
+  /* ------------------------- url ------------------------ */
+  if (req.url === "/") {
+    res.end("<h1>Welcome to our home page</h1>");
+  }
+  if (req.url === "/about") {
+    res.end("<h1>Here is our short history</h1>");
+  }
+  res.end(`
+    <h1>Oops!</h1>
+    <p>We can't seem to find the page you are looking for</p>
+    <a href="/"> Back home </a>
+`);
+  // createServer needs a Callback
+  // req is what is requested by user
+  // res is web response
+  /* -------------------- write method -------------------- */
+  //   res.write("Welcome to our homepage ");
+  /* --------------------- end method --------------------- */
+  // to send back our response
+  //   res.end();
+});
+
+//what port our server is listening to?
+/* -------------------- listen method ------------------- */
+server.listen(5000);
+// what do web servers do? keep on listening on request
+
+/* ------------------------------------------------------ */
+/*                           npm                          */ // #11
+/* ------------------------------------------------------ */
+// npm - global command, comes with node
+// npm --version
+
+/* ----------------- local dependency ---------------- */
+// use it only in this particular project
+// npm i <packageName>
+
+/* ------------------ global dependency ----------------- */
+// use it in any project
+// npm install -g <packageName>
+// sudo install -g <packageName> (mac)
+
+/* -------------------- package.json -------------------- */
+// manifest file (stores important info about project/package)
+
+/* ----------------- create package.json ---------------- */
+// manual approach (create package.json in the root, create properties etc)
+// npm init (step by step, press enter to skip step)
+// npm init -y (everything default)
+
+const _ = require("lodash");
+
+const items = [1, [2, [3, [4]]]];
+const newItems = _.flattenDeep(items);
+console.log(newItems);
+console.log("Hello  people");
+
+/* ------------------------------------------------------ */
+/*                       EVENT LOOP //EXAMPLES:           */ //#12
+/* ------------------------------------------------------ */
+
+/* --------------------- setTimeout --------------------- */
+// started operating system process
+console.log("first");
+setTimeout(() => {
+  console.log("second");
+}, 0);
+console.log("third");
+// completed and exited the operating system
+
+/* --------------------- setInterval -------------------- */
+setInterval(() => {
+  console.log("Hello  World!");
+}, 2000);
+console.log("I will run first!");
+//process stays alive unless
+// kill process control + c
+// unexpected error
+
+/* ----------------------- servers ---------------------- */
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log("Request Event");
+  res.end("hello Amir!");
+});
+
+server.listen(5000, () => {
+  // listen is Asyncronous and after we set it up
+  // eventloop just waiting fot requests and run the callback
+  console.log("Server listening on port : 5000....");
+});
--- app.js
+++ app.js
@@ -1,25 +1,21 @@
-// npm - global command, comes with node
-// npm --version
+const http = require("http");
 
-/* ----------------- local dependency ---------------- */
-// use it only in this particular project
-// npm i <packageName>
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("Home Page!");
+  } else if (req.url === "/about") {
+    /* -------------------- blocking code ------------------- */
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 12; j++) {
+        console.log(`${i} ${j}`);
+      }
+    }
+    res.end("About Page!");
+  } else {
+    res.end("Error Page!!!");
+  }
+});
 
-/* ------------------ global dependency ----------------- */
-// use it in any project
-// npm install -g <packageName>
-// sudo install -g <packageName> (mac)
-
-/* -------------------- package.json -------------------- */
-// manifest file (stores important info about project/package)
-
-/* ----------------- create package.json ---------------- */
-// manual approach (create package.json in the root, create properties etc)
-// npm init (step by step, press enter to skip step)
-// npm init -y (everything default)
-
-const _ = require("lodash");
-
-const items = [1, [2, [3, [4]]]];
-const newItems = _.flattenDeep(items);
-console.log(newItems);
+server.listen(4000, () => {
+  console.log("server listening on port: 4000...");
+});
